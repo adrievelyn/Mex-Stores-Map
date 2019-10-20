09f8402e4303dafafec1a8f3e79df0cd3a138052
Adriana Sanchez <adrievelyn@gmail.com>
2019-10-20 14:44:04 -0300

    agregando logo y navbar

---
+++ src/Components/Badge.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+class Badge extends React.Component {
+	render() { 
+		return <div>
+			<div> 
+				<img src='https://s4.aconvert.com/convert/p3r68-cdx67/b1peg-f7glb.svg' alt='logo'/>
+			</div>
+
+			<div> 
+				<h1>México Stores Map</h1>
+			</div>
+
+			<div> 
+				<h3>Visualiza las tiendas de México a través de nuestro Mapa</h3>
+			</div>
+
+			<div> 
+				<h5>MexStoresMap 2019 - Adriana Sánchez</h5>
+			</div>
+		</div>
+	}
+}
+
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';*/
+
+/*const element = document.createElement('h1');
+element.innerText = 'Bienvenido a Mapa de Adriana'
+
+const container = document.getElementById('app');
+
+container.appendChild(element);*/
+
+/*const element = <h1>Bienvenido a Mapa de Adriana</h1>;*/
+/*const element = React.createElement('h1', {}, 'Bienvenido a Mapa de Adriana');*/
+/*const element = React.createElement('a', {href: 'https://laboratoria.la'}, 'Ir a Laboratoria');*/
+/*const name = 'Tiendas de México';
+const sum = () => 3 + 4;*/
+/*const element = React.createElement('h1', {}, `Bienvenido a Mapa de ${name}`);*/
+/*const jsx = <h1>Bienvenido a Mapas de {_expression_}</h1> dentro de las llaves van expresiones que se pueden contabilizar con una variable*/
+/*si son falsos como = 0, undifined, vacio (no se van a reflejar)*/
+/*const jsx = <h1>Bienvenido a Mapas de {sum()}</h1>*/
+/*const jsx = <h1>Bienvenido a Mapas de {name}</h1>*/
+/*--------------------------------------------
+const jsx = (
+	<div>
+<h1>Hola, soy Adriana Sánchez</h1>
+<h2>Front-End Developer</h2>
+</div>
+);
+
+const element = React.createElement (
+'div',
+{},
+React.createElement('h1', {sum}, 'Hola, soy Adriana'),
+React.createElement('h2', {}, 'Soy Front-End Developer'),	
+);
+const container = document.getElementById('app');
+---------------------------------------------*/
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const element = (
+	<div>
+<h1>Hola, soy Adriana Sánchez</h1>
+<h2>Front-End Developer</h2>
+</div>
+);
+
+const container = document.getElementById('app');
+
+ReactDOM.render(element, container);
+----------------------------------------------- */
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Badge from './Components/Badge';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById('app');
+
+ReactDOM.render(<Badge />, container);
+
+
+
+
+
+
+
+
+
+
+
