061241b541e2dbc3ea14f0b104b90760a35fd731
adrievelyn <adrievelyn@gmail.com>
2019-10-24 01:34:57 -0300

    Updates

---
+++ precache-manifest.dec600232a76ccde605878a7dc772cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd314ef2d5af16181c7b5d9f88adf56b",
+    "url": "/Mex-Stores-Map/index.html"
+  },
+  {
+    "revision": "c5a984ef2700a5da7637",
+    "url": "/Mex-Stores-Map/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "3b2107906b1c8915d97d",
+    "url": "/Mex-Stores-Map/static/css/main.239732e6.chunk.css"
+  },
+  {
+    "revision": "c5a984ef2700a5da7637",
+    "url": "/Mex-Stores-Map/static/js/2.84afc587.chunk.js"
+  },
+  {
+    "revision": "3b2107906b1c8915d97d",
+    "url": "/Mex-Stores-Map/static/js/main.c266d61f.chunk.js"
+  },
+  {
+    "revision": "c9598a61fd5ccda553dc",
+    "url": "/Mex-Stores-Map/static/js/runtime-main.136de68b.js"
+  },
+  {
+    "revision": "6f957db0a9aa33454a9c773aa45490a5",
+    "url": "/Mex-Stores-Map/static/media/logo.6f957db0.svg"
+  }
+]);
